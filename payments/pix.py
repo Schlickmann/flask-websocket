27d35ac8027789dc97ff79b3f99891f5919584a6
Julian Schlickmann <juliani.schlickmann@gmail.com>
2025-08-18 19:30:26 -0700

    Add tests with pytest

--- payments/pix.py
+++ payments/pix.py
@@ -5,7 +5,7 @@ class Pix:
   def __init__(self):
     pass
 
-  def create_payment(self, amount: float):
+  def create_payment(self, base_dir=""):
     #  Create the payment in the banking system
     #  Return the bank_payment_id and the qr_code_path
     
@@ -18,7 +18,7 @@ class Pix:
     
     # qr code
     qr_code = qrcode.make(hash_payment)
-    qr_code.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+    qr_code.save(f"{base_dir}static/img/qr_code_payment_{bank_payment_id}.png")
 
     return {
       "bank_payment_id": bank_payment_id,
